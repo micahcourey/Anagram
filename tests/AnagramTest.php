9df895c1ff6e8247080e16f313c0a85602863d2c
Micah Courey <micahjcourey@gmail.com>
2015-08-11 16:53:53 -0700

    Update Test file

--- tests/AnagramTest.php
+++ tests/AnagramTest.php
@@ -1,40 +1,50 @@
 <?php
-
     require_once "src/Anagram.php";
-
     class AnagramTest extends PHPUnit_Framework_TestCase
     {
-
         function test_makeAnagram_specOne()
         {
             //Arrange
             $test_Anagram = new Anagram;
             $input1 = "A";
-            $input2 = "A";
-            var_dump($input2);
-
-
+            $input2 = array("A");
             //Act
             $result = $test_Anagram->makeAnagram($input1, $input2);
-
             //Assert
-            $this->assertEquals("A", $result);
+            $this->assertEquals(array("A"), $result);
         }
-
-        function test_makeAnagram_otherThing()
+        function test_makeAnagram_specTwo()
         {
             //Arrange
             $test_Anagram = new Anagram;
-            $input1 = "on";
-            $input2 = "no";
-            var_dump($input2);
-
+            $input1 = "doom";
+            $input2 = array("mood");
             //Act
             $result = $test_Anagram->makeAnagram($input1, $input2);
-
             //Assert
-            $this->assertEquals("no", $result);
+            $this->assertEquals(array("mood"), $result);
         }
-
+        function test_makeAnagram_specThree()
+        {
+            //Arrange
+            $test_Anagram = new Anagram;
+            $input1 = "doom";
+            $input2 = array("cat");
+            //Act
+            $result = $test_Anagram->makeAnagram($input1, $input2);
+            //Assert
+            $this->assertEquals(array(), $result);
+        }
+        function test_makeAnagram_specFour()
+        {
+            //Arrange
+            $test_Anagram = new Anagram;
+            $input1 = "doom";
+            $input2 = array("cat", "mood", "odom", "dog");
+            //Act
+            $result = $test_Anagram->makeAnagram($input1, $input2);
+            //Assert
+            $this->assertEquals(array("mood", "odom"), $result);
+        }
     }
 ?>
