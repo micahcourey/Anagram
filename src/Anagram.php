3e9fcd116d097b36db0e79c8ed8dda83c90b740e
Sarah McEwing & Micah Courey <student@epicodus.com>
2015-08-11 12:32:07 -0700

    add sort array

--- src/Anagram.php
+++ src/Anagram.php
@@ -5,6 +5,10 @@
 
         function makeAnagram($main_word, $check_words)
         {
+            $arr1 = str_split($main_word);
+            $arr2 = str_split($check_words);
+            sort($arr1);
+            sort($arr2);
             $output = '';
             if ($main_word === $check_words) {
                 $output = $check_words;
